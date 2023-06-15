297d5879ee9d0d33d6b22fb1829401d775c6aa72
Abdulmalik <oladimejimalik77@gmail.com>
2023-06-15 07:04:27 -0700

    Fix 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  *main - prints character in the printf function
  *Return: 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece art,\n");
