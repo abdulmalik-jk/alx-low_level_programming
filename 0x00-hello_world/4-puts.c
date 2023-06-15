e3e909eaef4eff87f8fb9dda7f49008ce8f2ebfe
Abdulmalik <oladimejimalik77@gmail.com>
2023-06-15 05:56:15 -0700

    Add a .c file containing c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ *main: prints the string in the put function
+return: 0 if succesful
+*/
+
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
