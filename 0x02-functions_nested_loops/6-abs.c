75cde97a9384ed9112078a767446d206119be5b6
Abdulmalik <oladimejimalik77@gmail.com>
2023-06-20 05:19:34 -0700

    Fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 /**
- *int_abs - computes absolute value of an integer
+ *_abs - computes absolute value of an integer
  *@n: integer whose absolute value is to be returned
  *Return: returns the absolute value
  */
