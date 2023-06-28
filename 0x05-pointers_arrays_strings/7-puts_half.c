e426fc93c8165668048947df5c891b7337900734
Abdulmalik <oladimejimalik77@gmail.com>
2023-06-27 17:22:43 -0700

    Fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,14 @@ void puts_half(char *str)
 		}
 		if (i % 2 != 0)
 		{
-			for (n = (i + 1) / 2; n < 1; n++)
+			for (n = (i + 1) / 2; n < i; n++)
 			{
 				_putchar(str[n]);
 			}
 		}
 			else
 			{
-				for (n = i / 2; n < 1; n++)
+				for (n = i / 2; n < i; n++)
 				{
 					_putchar(str[n]);
 				}
