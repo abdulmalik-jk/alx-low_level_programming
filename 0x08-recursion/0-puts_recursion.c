c300ca0fce5b052bf127668cc686eadaa39eb59f
Abdulmalik <oladimejimalik77@gmail.com>
2023-07-04 23:53:39 -0700

    Add task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts_recursion - prints a string followed by a new line
+ *@s: string to be printed
+ *
+ *Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	puts("Puts with recursion");
+}
