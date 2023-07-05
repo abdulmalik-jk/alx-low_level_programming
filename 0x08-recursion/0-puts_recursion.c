bf1a2161f99646ca8f894b548eb5f30330037bbd
Abdulmalik <oladimejimalik77@gmail.com>
2023-07-04 23:58:18 -0700

    Fix task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *_puts_recursion - prints a string followed by a new line
@@ -10,5 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-	puts("Puts with recursion");
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
