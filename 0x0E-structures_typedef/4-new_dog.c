ab5bdf21442fa2e41747e5d3e6f235b860856a51
Abdulmalik <oladimejimalik77@gmail.com>
2023-07-17 02:08:38 -0700

    tasks

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+ * new_dog - new dog function
+ * @name: dog name
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+ * Return: NULL on failure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *a, *b;
+	int i = 0, j = 0, k, l;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	while (name[i] != '\0')
+	{
+		i++;
+	}
+	while (owner[j] != '\0')
+	{
+		j++;
+	}
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	a = malloc(sizeof(char) * i + 1);
+	if (a == NULL)
+	{
+		free(new_dog);
+		free(a);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		a[k] = name[k];
+	for (l = 0; l <= j; i++)
+		b[l] = owner[l];
+	new_dog->name = a;
+	new_dog->age = age;
+	new_dog->owner = b;
+	return (new_dog);
+}
