f934e0586a976d166c590d2a076f4818dbc7cccb
Abdulmalik <oladimejimalik77@gmail.com>
2023-07-27 11:19:05 -0700

    task 0?

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -30,26 +30,4 @@ size_t print_list(const list_t *h)
 	}
 	return (n);
 }
-/**
-*	if (h == NULL)
-*	{
-*		return (0);
-*	}
-*	while (h != NULL)
-*	{
-*		if (h->str == NULL)
-*		{
-*			printf("[0] ");
-*			printf("(nil)\n");
-*		}
-*		else
-*		{
-*			printf("[%i] ", h->len);
-*			printf("%s\n ", h->str);
-*		}
-*		n++;
-*		h = h->next;
-*	}
-*	return (n);
-*}
-*/
+
