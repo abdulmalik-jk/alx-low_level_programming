1181f8443dd80725dafb2fd9160fc6841a850ead
Abdulmalik <oladimejimalik77@gmail.com>
2023-07-27 12:50:30 -0700

    fix task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node - add node to head of alinked list
@@ -7,36 +8,70 @@
  *
  * Return: pointer to new head
  */
-
 list_t *add_node(list_t **head, const char *str)
+/**
+*	list_t *new;
+*	int i;
+*	char *cp_s;
+*
+*	new = malloc(sizeof(list_t));
+*	if (new == NULL)
+*	{
+*		return (NULL);
+*	}
+*
+*	cp_s = strdup(str);
+*	if (cp_s == NULL)
+*	{
+*		free(new);
+*		return (NULL);
+*	}
+*
+*
+*	i = 0;
+*	while (str[i])
+*	{
+*		i++;
+*	}
+*
+*	new->len = i;
+*	new->str = cp_s;
+*	new->next = *head;
+*	*head = new;
+*
+*	return (new);
+*}
+*/
 {
 	list_t *new;
-	int i;
-	char *cp_s;
+
+	char *str_dup;
+
+	int len;
 
 	new = malloc(sizeof(list_t));
+
 	if (new == NULL)
 	{
 		return (NULL);
 	}
+	str_dup = strdup(str);
 
-	cp_s = strdup(str);
-	if (cp_s == NULL)
+	if (str_dup == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 
-
-	i = 0;
-	while (str[i])
+	len = 0;
+	while (str[len])
 	{
-		i++;
+		len++;
 	}
-
-	new->len = i;
-	new->str = cp_s;
+	new->str = str_dup;
+	new->len = len;
 	new->next = *head;
+
 	*head = new;
 
 	return (new);
