c8c27936f8872fdc50e5fa2cd2340b79e2a0d2f2
Abdulmalik <oladimejimalik77@gmail.com>
2023-08-03 16:02:31 -0700

    fix task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,8 +13,11 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int check = 1;
 
 	if (index > 64)
+	{
 		return (-1);
+	}
 	check = check << index;
 	*n = *n | check;
+
 	return (-1);
 }
